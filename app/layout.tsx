e9956843e53e4d3e91ea0816735f6e5644174335
Nodar Miminoshvili <miminoshvilinodar@gmail.com>
2024-08-03 11:03:51 +0400

    Add Navbar for mobile devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Manrope } from 'next/font/google';
 import './globals.css';
+import Header from './components/Header/Navbar';
 
 const manrope = Manrope({
   weight: ['400', '500', '600', '700'],
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={manrope.className}>{children}</body>
+      <body className={manrope.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
